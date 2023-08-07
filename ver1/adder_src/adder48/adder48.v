2cbab420d6ad244adc15aeacfcdf8979826e99d6
Candiwind <66230426+MJ-zrz@users.noreply.github.com>
2023-08-07 15:07:03 +0800

    Add files via upload

--- ver1/adder_src/adder48/adder48.v
+++ ver1/adder_src/adder48/adder48.v
@@ -4,11 +4,9 @@ module adder48(
     input           [48:1]      B                 ,
     input                       c0                ,
     output          [48:1]      S                 ,
-    output                      c48               ,
-    output                      sx                
+    output                      c48                     
 );
 
-    wire sx1, sx2, sx3;
     wire c16, c32;
 
     adder16 adder16_inst0(
@@ -16,8 +14,7 @@ module adder48(
         .B              (B[16:1]    )       ,
         .c0             (c0         )       ,
         .S              (S[16:1]    )       ,
-        .c16            (c16        )       ,
-        .sx             (sx1        )       
+        .c16            (c16        )       
     );
 
     adder16 adder16_inst1(
@@ -25,8 +22,7 @@ module adder48(
         .B              (B[32:17]   )       ,
         .c0             (c16        )       ,
         .S              (S[32:17]   )       ,
-        .c16            (c32        )       ,
-        .sx             (sx2        )       
+        .c16            (c32        )            
     );
 
     adder16 adder16_inst2(
@@ -34,23 +30,7 @@ module adder48(
         .B              (B[48:33]   )       ,
         .c0             (c32        )       ,
         .S              (S[48:33]   )       ,
-        .c16            (c48        )       ,
-        .sx             (sx3        )       
-    );
-
-    CC3 CC3_inst(
-        .c0             (c0             )       ,
-        .s1             (sx1            )       ,
-        .s2             (sx2            )       ,
-        .s3             (sx3            )       ,
-        .d1             (c0             )       ,
-        .d2             (c16            )       ,
-        .d3             (c32            )       ,
-        .sx             (sx             )       ,
-        .sum            ()                      ,
-        .c1             ()                      ,
-        .c2             ()                      ,
-        .c3             (c48)                      
+        .c16            (c48        )       
     );
 
 
